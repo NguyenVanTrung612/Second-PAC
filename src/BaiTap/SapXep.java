3c8b4d803b0fef55dea48cd397168e39d514c00c
NguyenVanTrung612 <vantrung060101@gmail.com>
2021-01-28 17:10:51 +0700

    change

---
+++ src/BaiTap/SapXep.java
@@ -0,0 +1,25 @@
+package BaiTap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class SapXep {
+    public static void main(String[] args) {
+        List<String> listString = new ArrayList<String>();
+
+        listString.add("a");
+        listString.add("b");
+        listString.add("c");
+        listString.add("d");
+
+        for (String s : listString){
+            System.out.println(s);
+        }
+
+
+
+
+
+    }
+    
+}
