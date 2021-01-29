96a15603bef55f513f2e8b71ef30e3a4c7895cc5
NguyenVanTrung612 <vantrung060101@gmail.com>
2021-01-29 17:06:04 +0700

    change

--- src/BaiTap/HinhVuong.java
+++ src/BaiTap/HinhVuong.java
@@ -1,18 +1,5 @@
 package BaiTap;
 
 public class HinhVuong {
-    public static float chuVi(float canhHinhVuong){
-        return canhHinhVuong * 4;
-    }
-     public static float dienTich(float canhHinhVuong){
-          return canhHinhVuong *canhHinhVuong;
-     }
-      public static void main(String[] args) {
-        float chieuDai =4.5f;
-         System.out.println("Chui vi va Dien tich hinh vuong");
-          System.out.println(HinhVuong.chuVi(chieuDai));
-           System.out.println(HinhVuong.dienTich(chieuDai));
-         
-     }
-    
+   
 }
