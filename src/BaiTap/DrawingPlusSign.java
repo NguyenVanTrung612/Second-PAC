23212a17a91e57b5556147ae967d361230f2674b
NguyenVanTrung612 <vantrung060101@gmail.com>
2021-02-01 12:24:40 +0700

    init

--- src/BaiTap/DrawingPlusSign.java
+++ src/BaiTap/DrawingPlusSign.java
@@ -6,13 +6,18 @@ public class DrawingPlusSign {
             for ( int j = 1; i<=10; i++) {
                 if(((i == 1 || i == 10 || i == 4 || i == 7) && (j == 4 || j == 7))
                 || ((j == 1 || j == 10) && (i == 4 || i == 7))) {
-                    System.out.println("*");
+                    System.out.print("*");
                 }else if ((i == 1 || j == 10) && (i == 5 || i == 6)
-                 || (i == 2 || i == 3) && (j == 4 || j == 7)) {
-                    System.out.println("|");
+                 || (i == 2 || i == 3 || i == 8 || i == 9) && (j == 4 || j == 7)) {
+                    System.out.print("|");
+                }else if ((i == 1 || i == 10) && (j == 5 || j == 6)
+                 || (i == 4 || i == 7) && (j == 2 || j == 3 || j == 8 || j == 9)) {
+                    System.out.print("-");
+                }else {
+                    System.out.print(" ");
                 }
             }
-        
+              System.out.println();
 
         }
     }
